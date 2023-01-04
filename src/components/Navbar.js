b5fc4b446ff4d9f7d1a3bc0120cfd932f4fd9727
Mayanja Andrew <andremayanja@gmail.com>
2023-01-04 17:07:23 +0300

    dark mode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useTheme } from "../hooks/useTheme";
+import { useContext } from "react";
 import "./Navbar.css";
 import Searchbar from "./Searchbar";
+import { ThemeContext } from "../context/ThemeContext";
 export default function Navbar() {
+  const { color, changeColor } = useTheme();
   return (
-    <div className="navbar">
-      <nav>
+    <div className="navbar" style={{ background: color }}>
+      <nav onClick={() => changeColor("pink")}>
         <Link to="/" className="brand">
           <h1>Cooking ninja</h1>
         </Link>
